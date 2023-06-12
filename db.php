e8c243d28dbf424627ee0a3f48a2c4d01a627f96
M Raffli Arraqina Nugroho <110365513+raffliarraqina@users.noreply.github.com>
2023-06-12 10:37:20 +0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$host_name = 'localhost';
+$db_user = 'ryurafli_arraqina';
+$db_pass = '37De53SWF9pXEAE';
+$db_name = 'ryurafli_arraqina';
+
+$con = mysqli_connect($host_name,$db_user,$db_pass,$db_name);
+
+?>
